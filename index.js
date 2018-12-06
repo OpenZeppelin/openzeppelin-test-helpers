455bf542e87712d284428de97e5385ec38db1e78
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-12-06 14:11:17 -0300

    Added index.js exporting all submodules.

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  balanceDifference: require('./src/balanceDifference'),
+  constants: require('./src/constants'),
+  ether: require('./src/ether'),
+  expectEvent: require('./src/expectEvent'),
+  makeInterfaceId: require('./src/makeInterfaceId'),
+  send: require('./src/send'),
+  shouldFail: require('./src/shouldFail'),
+  time: require('./src/time'),
+  web3: require('./src/web3'),
+};
