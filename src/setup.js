8ff012fad4b2c2f90bcdb9e36ef9abde281d41eb
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-12-07 16:49:31 -0300

    Added basic web3 detection.

--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,14 @@
 const chai = require('chai');
-const BigNumber = web3.BigNumber;
-const should = chai.use(require('chai-bignumber')(BigNumber)).should();
+const semver = require('semver');
+
+// Different web3 versions store the version string in different places
+const web3Version = web3.version.api || web3.version;
+
+const BigNumber = semver.satisfies(web3Version, '<1.0.0') ? web3.BigNumber : web3.util.BN;
+
+const should = chai
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 module.exports = {
   BigNumber,
