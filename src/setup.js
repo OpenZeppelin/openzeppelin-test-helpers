0fed832079ae42da5ce8f0ff7e1f7bc801ed4db7
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-12-28 11:14:29 -0300

    Drop support for pre 1.0 web3.

--- src/setup.js
+++ src/setup.js
@@ -1,16 +1,12 @@
 const chai = require('chai');
-const semver = require('semver');
 
-// Different web3 versions store the version string in different places
-const web3Version = web3.version.api || web3.version;
-
-const BigNumber = semver.satisfies(web3Version, '<1.0.0') ? web3.BigNumber : web3.utils.BN;
+const BN = web3.utils.BN;
 
 const should = chai
-  .use(require('chai-bignumber')(BigNumber))
+  .use(require('chai-bignumber')(BN))
   .should();
 
 module.exports = {
-  BigNumber,
+  BN,
   should,
 };
