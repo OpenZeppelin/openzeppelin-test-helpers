9cebdd290b5394653185a168b89e258b0c95fd28
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-01-02 18:20:55 -0300

    Add balance.current.

--- src/balance.js
+++ src/balance.js
@@ -1,5 +1,9 @@
 const { BN } = require('./setup');
 
+async function balanceCurrent (account) {
+  return new BN(await web3.eth.getBalance(account));
+}
+
 async function balanceDifference (account, promiseFunc) {
   const balanceBefore = new BN(await web3.eth.getBalance(account));
   await promiseFunc();
@@ -8,5 +12,6 @@ async function balanceDifference (account, promiseFunc) {
 }
 
 module.exports = {
+  current: balanceCurrent,
   difference: balanceDifference,
 };
--- test/src/balance.test.js
+++ test/src/balance.test.js
@@ -5,6 +5,12 @@ const send = require('../../src/send');
 const ether = require('../../src/ether');
 
 contract('balance', function ([sender, receiver]) {
+  describe('current', function () {
+    it('returns the current balance of an account as a BN', async function () {
+      (await balance.current(sender)).should.be.bignumber.equal(await web3.eth.getBalance(sender));
+    });
+  });
+
   describe('difference', function () {
     it('returns balance increments', async function () {
       (await balance.difference(receiver, () =>
