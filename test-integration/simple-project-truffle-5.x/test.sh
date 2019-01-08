66c173df16744eca543b7ddd7e514773a3dffa72
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-01-08 13:57:44 -0300

    Improved truffle 5 integration tests.

--- test-integration/simple-project-truffle-5.x/test.sh
+++ test-integration/simple-project-truffle-5.x/test.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Delete the old symlink, if it existed
-rm -rf node_modules/openzeppelin-test-helpers
-
-npm install
-
 # Delete the installed dependency
 rm -rf node_modules/openzeppelin-test-helpers
 # Replace it with the local package
 ln -s ../../.. node_modules/openzeppelin-test-helpers
 
-npm test
+npx truffle test
--- test-integration/simple-project-truffle-5.x/test/Tested.test.js
+++ test-integration/simple-project-truffle-5.x/test/Tested.test.js
@@ -1,14 +1,28 @@
+const { BN, constants, expectEvent, shouldFail } = require('openzeppelin-test-helpers');
+
 const Tested = artifacts.require('Tested');
 
-const BigNumber = web3.BigNumber;
-require('chai')
-  .use(require('chai-bignumber')(BigNumber))
-  .should();
-
-const { expectEvent } = require('openzeppelin-test-helpers');
-
 contract('Tested', function (accounts) {
-  it('can be deployed', async function () {
-    await expectEvent.inConstruction(await Tested.new(), 'Constructed');
+  it('detects events during construction', async function () {
+    await expectEvent.inConstruction(await Tested.new(3), 'Constructed', { value: new BN(3) });
+  });
+
+  context('with deployed instance', function () {
+    beforeEach(async function () {
+      this.contract = await Tested.new(0);
+    })
+
+    it('detect reverts', async function () {
+      await shouldFail.reverting(this.contract.reverts());
+    });
+
+    it('accepts calls with non-zero address', async function () {
+      const { logs } = await this.contract.nonZeroAddress(accounts[0]);
+      expectEvent.inLogs(logs, 'Address', { account: accounts[0] });
+    });
+
+    it('reverts with calls with non-zero address', async function () {
+      await shouldFail.reverting(this.contract.nonZeroAddress(constants.ZERO_ADDRESS));
+    });
   });
 });
