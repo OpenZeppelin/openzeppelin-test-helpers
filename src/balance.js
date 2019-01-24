135bad0e2b6b47167cb6ce7a52697aec6f692919
JG Carvalho <jgcarvalho93@gmail.com>
2019-01-24 17:45:45 -0200

    intial implementation

--- src/balance.js
+++ src/balance.js
@@ -1,17 +1,27 @@
 const { BN } = require('./setup');
 
-async function balanceCurrent (account) {
-  return new BN(await web3.eth.getBalance(account));
+function Tracker(acc){
+  this.balances = [];
+  this.account = acc;
+  this.delta = async function() {
+    let current = await this.get();
+    return current.sub(this.balances[this.balances.length - 2]);
+  },
+  this.intial = function(){
+    return this.balances[0];
+  },
+
+  this.get = async function() {
+    let bal = new BN(await web3.eth.getBalance(this.account));
+    this.balances.push(bal);
+    return bal;
+  }
 }
 
-async function balanceDifference (account, promiseFunc) {
-  const balanceBefore = new BN(await web3.eth.getBalance(account));
-  await promiseFunc();
-  const balanceAfter = new BN(await web3.eth.getBalance(account));
-  return balanceAfter.sub(balanceBefore);
+async function balanceTracker(owner) {
+  let tracker = new Tracker(owner);
+  await tracker.get();
+  return tracker;
 }
 
-module.exports = {
-  current: balanceCurrent,
-  difference: balanceDifference,
-};
+module.exports = balanceTracker
--- test/src/balance.test.js
+++ test/src/balance.test.js
@@ -7,21 +7,29 @@ const ether = require('../../src/ether');
 contract('balance', function ([sender, receiver]) {
   describe('current', function () {
     it('returns the current balance of an account as a BN', async function () {
-      (await balance.current(sender)).should.be.bignumber.equal(await web3.eth.getBalance(sender));
+      let tracker = await balance(sender);
+      (await tracker.get()).should.be.bignumber.equal(await web3.eth.getBalance(sender));
     });
   });
 
   describe('difference', function () {
     it('returns balance increments', async function () {
-      (await balance.difference(receiver, () =>
-        send.ether(sender, receiver, ether('1')))
-      ).should.be.bignumber.equal(ether('1'));
+      let tracker = await balance(receiver);
+      await send.ether(sender, receiver, ether('1'));
+      (await tracker.delta()).should.be.bignumber.equal(ether('1'));
     });
 
     it('returns balance decrements', async function () {
-      (await balance.difference(sender, () =>
-        send.ether(sender, receiver, ether('1')))
-      ).should.be.bignumber.equal(ether('-1'));
+      let tracker = await balance(sender);
+      await send.ether(sender, receiver, ether('1'));
+      (await tracker.delta()).should.be.bignumber.equal(ether('-1'));
     });
+
+    it('returns consecutive deltas', async function () {
+      let tracker = await balance(sender);
+      await send.ether(sender, receiver, ether('1'));
+      let d1 = await tracker.delta();
+      (await tracker.delta()).should.be.bignumber.equal('0');
+    })
   });
 });
