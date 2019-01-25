79c27f07b5d02b93fb660d851e76cedc15e8e278
JG Carvalho <jgcarvalho93@gmail.com>
2019-01-25 13:56:40 -0200

    small refactoring

--- src/balance.js
+++ src/balance.js
@@ -1,19 +1,15 @@
 const { BN } = require('./setup');
 
 function Tracker(acc){
-  this.balances = [];
-  this.account = acc;
+  let balances = [];
+  let account = acc;
   this.delta = async function() {
     let current = await this.get();
-    return current.sub(this.balances[this.balances.length - 2]);
+    return current.sub(balances[balances.length - 2]);
   },
-  this.intial = function(){
-    return this.balances[0];
-  },
-
   this.get = async function() {
-    let bal = new BN(await web3.eth.getBalance(this.account));
-    this.balances.push(bal);
+    let bal = new BN(await web3.eth.getBalance(account));
+    balances.push(bal);
     return bal;
   }
 }
