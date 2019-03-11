b0fe51b90e08199a7f34d278daf1af4ba5478a93
Joao Gabriel Carvalho <jg@jgcarv.com>
2019-03-11 19:58:11 -0300

    Proposal for balance redesign (#13)
    * intial implementation
    
    * small refactoring
    
    * changed readme
    
    * implemente pr comments
    
    * fixed linter errors
    
    * addressed remaning comments
    
    * fixed integration test
    
    * removed array usage
    
    * using balanceCurrent for fetching tracker balance
    
    * fixed small issue in test

--- test-integration/simple-project-truffle-5.x/test/accounts.test.js
+++ test-integration/simple-project-truffle-5.x/test/accounts.test.js
@@ -3,9 +3,8 @@ const { balance, BN, ether, send } = require('openzeppelin-test-helpers');
 contract('accounts', function (accounts) {
   it('sends ether and tracks balances', async function () {
     const value = ether('42', 'ether');
-
-    (await balance.difference(accounts[0], () =>
-      send.ether(accounts[0], accounts[1], value)
-    )).should.be.bignumber.equals(value.neg());
+    const tracker = await balance.tracker(accounts[0]);
+    await send.ether(accounts[0], accounts[1], value);
+    (await tracker.delta()).should.be.bignumber.equals(value.neg());
   });
 });
--- test/src/balance.test.js
+++ test/src/balance.test.js
@@ -11,17 +11,44 @@ contract('balance', function ([sender, receiver]) {
     });
   });
 
-  describe('difference', function () {
+  describe('balance tracker', function () {
+    it('returns current balance ', async function () {
+      const tracker = await balance.tracker(receiver);
+      (await tracker.get()).should.be.bignumber.equal(await web3.eth.getBalance(receiver));
+    });
+
+    it('get() adds a new checkpoint ', async function () {
+      const tracker = await balance.tracker(sender);
+      await send.ether(sender, receiver, ether('1'));
+      await tracker.get();
+      (await tracker.delta()).should.be.bignumber.equal('0');
+    });
+
+    it('returns correct deltas after get() checkpoint', async function () {
+      const tracker = await balance.tracker(receiver);
+      await send.ether(sender, receiver, ether('1'));
+      await tracker.get();
+      await send.ether(sender, receiver, ether('1'));
+      (await tracker.delta()).should.be.bignumber.equal(ether('1'));
+    });
+
     it('returns balance increments', async function () {
-      (await balance.difference(receiver, () =>
-        send.ether(sender, receiver, ether('1')))
-      ).should.be.bignumber.equal(ether('1'));
+      const tracker = await balance.tracker(receiver);
+      await send.ether(sender, receiver, ether('1'));
+      (await tracker.delta()).should.be.bignumber.equal(ether('1'));
     });
 
     it('returns balance decrements', async function () {
-      (await balance.difference(sender, () =>
-        send.ether(sender, receiver, ether('1')))
-      ).should.be.bignumber.equal(ether('-1'));
+      const tracker = await balance.tracker(sender);
+      await send.ether(sender, receiver, ether('1'));
+      (await tracker.delta()).should.be.bignumber.equal(ether('-1'));
+    });
+
+    it('returns consecutive deltas', async function () {
+      const tracker = await balance.tracker(sender);
+      await send.ether(sender, receiver, ether('1'));
+      await tracker.delta();
+      (await tracker.delta()).should.be.bignumber.equal('0');
     });
   });
 });
