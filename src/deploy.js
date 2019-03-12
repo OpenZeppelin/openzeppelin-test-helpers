f110d91adf46416df40f89b2e9ca51f7bae5bdcd
Francisco Giordano <frangio.1@gmail.com>
2019-03-12 12:45:09 -0300

    Update src/deploy.js
    Co-Authored-By: nventuro <nicolas.venturo@gmail.com>

--- src/deploy.js
+++ src/deploy.js
@@ -9,7 +9,7 @@ async function ERC1820Registry (from) {
   // Read https://eips.ethereum.org/EIPS/eip-1820 for more information as to how the ERC1820 registry is deployed to
   // ensure its address is the same on all chains.
 
-  // 0.08 Ether is needed to deploy the registry, and those funds need to be transferred to the account that will deploy
+  // 0.08 ether is needed to deploy the registry, and those funds need to be transferred to the account that will deploy
   // the contract.
   await send.ether(from, '0x5808bA8E60E0367C9067b328D75C1f3d29de58cf', ether('0.08'));
 
