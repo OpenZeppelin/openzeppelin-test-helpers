c441f9c6f29c2d165d729005afdc2d0ac4d39cd3
Francisco Giordano <frangio.1@gmail.com>
2019-05-09 13:14:18 -0300

    rename main file to index.js

--- test-integration/ganache-core-2.1.x/test.sh
+++ test-integration/ganache-core-2.1.x/test.sh
@@ -4,7 +4,7 @@
 rm -rf node_modules/openzeppelin-test-helpers
 # Replace it with the local package
 mkdir -p node_modules/openzeppelin-test-helpers/src
-cp -r ../../openzeppelin-test-helpers.js node_modules/openzeppelin-test-helpers/
+cp -r ../../index.js node_modules/openzeppelin-test-helpers/
 cp -r ../../package.json node_modules/openzeppelin-test-helpers/
 cp -r ../../src/* node_modules/openzeppelin-test-helpers/src/
 
