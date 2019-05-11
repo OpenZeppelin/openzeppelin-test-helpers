f5bf20a86bc453ed137abdc1244d5eb772d57b86
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-05-10 21:17:01 -0300

    Fix exports.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   expectEvent: require('./src/expectEvent'),
   makeInterfaceId: require('./src/makeInterfaceId'),
   send: require('./src/send'),
-  shouldFail: require('./src/shouldFail'),
+  expectFailure: require('./src/expectFailure'),
   singletons: require('./src/singletons'),
   time: require('./src/time'),
 };
