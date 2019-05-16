39327616faffdb9cabc1a089630a668ea8b0dc74
Francisco Giordano <frangio.1@gmail.com>
2019-05-16 11:29:22 -0300

    fix lint errors

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const { BN } = require('./src/setup');
 
 module.exports = {
   BN,
-  get balance() { return require('./src/balance') },
-  get constants() { return require('./src/constants') },
-  get ether() { return require('./src/ether') },
-  get expectEvent() { return require('./src/expectEvent') },
-  get makeInterfaceId() { return require('./src/makeInterfaceId') },
-  get send() { return require('./src/send') },
-  get shouldFail() { return require('./src/shouldFail') },
-  get singletons() { return require('./src/singletons') },
-  get time() { return require('./src/time') },
+  get balance () { return require('./src/balance'); },
+  get constants () { return require('./src/constants'); },
+  get ether () { return require('./src/ether'); },
+  get expectEvent () { return require('./src/expectEvent'); },
+  get makeInterfaceId () { return require('./src/makeInterfaceId'); },
+  get send () { return require('./src/send'); },
+  get shouldFail () { return require('./src/shouldFail'); },
+  get singletons () { return require('./src/singletons'); },
+  get time () { return require('./src/time'); },
 };
