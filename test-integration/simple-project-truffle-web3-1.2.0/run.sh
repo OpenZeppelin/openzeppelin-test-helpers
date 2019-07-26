95a5212a1e95a901795d445377a7902d5bf13f3d
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-26 14:27:23 -0300

    Apply review suggestions

--- test-integration/simple-project-truffle-web3-1.2.0/run.sh
+++ test-integration/simple-project-truffle-web3-1.2.0/run.sh
@@ -6,6 +6,7 @@ cd "$(dirname $0)"
 
 cp -r ../simple-project-truffle-template .
 
-sed -i "s/\"truffle\": \"5.0.1\"/\"truffle\": \"5.0.29\"/g" simple-project-truffle-template/package.json
+cd simple-project-truffle-template
+npm install truffle@5.0.29
 
-./simple-project-truffle-template/run.sh
+./run.sh
