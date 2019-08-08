d0e69ea11b59f0b44349ec27fbf130cc5e9ef492
Francisco Giordano <frangio.1@gmail.com>
2019-08-07 22:12:01 -0300

    add explanation for regex

--- src/expectRevert.js
+++ src/expectRevert.js
@@ -9,6 +9,8 @@ async function expectException (promise, expectedError) {
     await promise;
   } catch (error) {
     if (error.message.indexOf(expectedError) === -1) {
+      // When the exception was a revert, the resulting string will include only
+      // the revert reason, otherwise it will be the type of exception (e.g. 'invalid opcode')
       const actualError = error.message.replace(/Returned error: VM Exception while processing transaction: (revert )?/, '');
       expect(actualError).to.equal(expectedError, `Wrong kind of exception received`);
     }
