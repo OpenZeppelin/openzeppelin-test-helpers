b5e269e773299cd2a1e692d977222ff348e67d84
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-10-07 18:40:08 -0300

    Scope npm package

--- test-integration/truffle-migration/migrations/2_test_send.js
+++ test-integration/truffle-migration/migrations/2_test_send.js
@@ -1,5 +1,5 @@
 module.exports = async function(deployer, network, accounts) {
-  const { send } = require('openzeppelin-test-helpers');
+  const { send } = require('@openzeppelin/test-helpers');
 
   await send.ether(accounts[0], accounts[1], 1);
 
