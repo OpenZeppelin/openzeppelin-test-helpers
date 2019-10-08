b2bb4e8fd436aca9c0dbe2b741708f69ea8a4673
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-10-08 18:10:19 -0300

    Add optional 'units' argument to balance functions

--- src/balance.js
+++ src/balance.js
@@ -1,29 +1,33 @@
 const { web3, BN } = require('./setup');
+const { fromWei } = require('web3-utils');
 
 class Tracker {
-  constructor (acc) {
+  constructor (acc, units) {
     this.account = acc;
+    this.units = units;
   }
-  async delta () {
+  async delta (units=this.units) {
     const current = await balanceCurrent(this.account);
     const delta = current.sub(this.prev);
     this.prev = current;
-    return delta;
+
+    return new BN(fromWei(delta, units));
   }
-  async get () {
+  async get (units=this.units) {
     this.prev = await balanceCurrent(this.account);
-    return this.prev;
+
+    return new BN(fromWei(this.prev, units));
   }
 }
 
-async function balanceTracker (owner) {
-  const tracker = new Tracker(owner);
+async function balanceTracker (owner, units='wei') {
+  const tracker = new Tracker(owner, units);
   await tracker.get();
   return tracker;
 }
 
-async function balanceCurrent (account) {
-  return new BN(await web3.eth.getBalance(account));
+async function balanceCurrent (account, units='wei') {
+  return new BN(fromWei(await web3.eth.getBalance(account), units));
 }
 
 module.exports = {
