0122a94cca5bd2bd317b72eaf71bf98234a8db25
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-10-09 16:53:15 -0300

    Improve node mismatch warnings

--- src/expectRevert.js
+++ src/expectRevert.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 const colors = require('ansi-colors');
 const semver = require('semver');
 
-let nodeInfo; // We cache the result for performance
+const checkedProviders = new WeakSet();
 
 async function expectException (promise, expectedError) {
   try {
@@ -25,34 +25,51 @@ async function expectException (promise, expectedError) {
   expect.fail('Expected an exception but none was received');
 }
 
+async function checkRevertReasonSupport (provider) {
+  if (!checkedProviders.has(provider)) {
+    // Find out if the provider supports revert reasons.
+    // Implementations with known support:
+    //  * ganache-core (TestRPC) >=2.2.0 (https://github.com/trufflesuite/ganache-core/releases/tag/v2.2.0)
+
+    const nodeInfo = await web3.eth.getNodeInfo();
+    const ganacheVersion = /TestRPC\/v([\w.-]+)\/ethereum-js/.exec(nodeInfo);
+
+    const warn = function (msg) {
+      console.log(`\
+${colors.white.bgBlack('openzeppelin-test-helpers')} ${colors.black.bgYellow('WARN')} expectRevert: ${msg}`
+      );
+    };
+
+    if (ganacheVersion === null) {
+      warn(`\
+Assertions may yield false negatives!
+
+Revert reason checks are only known to work on Ganache >=2.2.0, and the current node is ${nodeInfo}.
+
+If your node does support revert reasons, please let us know: \
+https://github.com/OpenZeppelin/openzeppelin-test-helpers/issues/new`
+      );
+    } else if (!semver.gte(ganacheVersion[1], '2.2.0')) {
+      throw new Error(`\
+The current version of Ganache (v${ganacheVersion[1]}) doesn't return revert reasons.
+
+Upgrade to v2.2.0 or newer, or use expectRevert.unspecified to skip the revert reason check.`
+      );
+    }
+
+    checkedProviders.add(provider);
+  }
+}
+
 const expectRevert = async function (promise, expectedError) {
+  promise.catch(() => { }); // Avoids uncaught promise rejections in case an input validation causes us to return early
+
   if (!expectedError) {
-    promise.catch(() => { });
     throw Error('No revert reason specified: call expectRevert with the reason string, or use expectRevert.unspecified \
 if your \'require\' statement doesn\'t have one.');
   }
 
-  // Find out if current version of ganache-core supports revert reason i.e >= 2.2.0.
-  // https://github.com/trufflesuite/ganache-core/releases/tag/v2.2.0
-  if (nodeInfo === undefined) {
-    nodeInfo = await web3.eth.getNodeInfo();
-  }
-  const matches = /TestRPC\/v([\w.-]+)\/ethereum-js/.exec(nodeInfo);
-
-  const warn = function (msg) {
-    console.log(`${colors.white.bgBlack('openzeppelin-test-helpers')} ${colors.black.bgYellow('WARN')} \
-      expectRevert: ` + msg);
-  };
-
-  if (matches === null || !(1 in matches)) {
-    // warn users and skip reason check.
-    warn('revert reason checking only supported on Ganache v2.2.0 or newer.');
-    expectedError = 'revert';
-  } else if (!semver.gte(matches[1], '2.2.0')) {
-    // warn users and skip reason check.
-    warn(`current version of Ganache (v${matches[1]}) doesn't return revert reason. Use v2.2.0 or newer.`);
-    expectedError = 'revert';
-  }
+  await checkRevertReasonSupport(web3.currentProvider);
 
   await expectException(promise, expectedError);
 };
