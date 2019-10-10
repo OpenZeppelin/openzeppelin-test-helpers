533b721319166d777cab9d0a3ed42c81caf764c4
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-10-10 13:30:20 -0300

    Make truffle detection and configuration more robust

--- src/config/environment.js
+++ src/config/environment.js
@@ -7,12 +7,19 @@ function setEnvironment (env) {
     throw new Error(`Unknown runtime environment '${env}'`);
   }
 
+  if (env === 'truffle' && !isTruffleEnvironment()) {
+    throw new Error(`\
+Attempt to configure runtime environment to 'truffle', but truffle detection failed.
+
+Are you running 'truffle test' or 'truffle exec'?`
+    );
+  }
+
   environment = env;
 }
 
 function setDefaultEnvironment () {
-  // If there is a (truffle-injected) global web3 instance, assume we're in a truffle environment
-  if (typeof web3 !== 'undefined') {
+  if (isTruffleEnvironment()) {
     setEnvironment('truffle');
   } else {
     setEnvironment('web3');
@@ -25,6 +32,11 @@ function getEnviroment () {
   }
 
   return environment;
+}
+
+function isTruffleEnvironment () {
+  // Truffle environments are detected by the presence of (truffle-injected) global web3 and artifacts variables
+  return (typeof web3 !== 'undefined' && typeof artifacts !== 'undefined');
 }
 
 setEnvironment.default = setDefaultEnvironment;
